69af809af6ffb67c90653816a31f4a17d4223d71
AlisonGrant <grantalison133@gmail.com>
2016-03-15 10:31:56 -0400

    js

--- js/getFooter.js
+++ js/getFooter.js
@@ -3,7 +3,7 @@ $.get("http://AlisonGrant.github.io/partials/footer.html", function(data) {
   $(document).ready(function() {
 
     $(".container").append(data);
-    //$(".container").fadeIn();
+    $(".container").fadeIn();
 
   })
 
