294fcf620cf2343d7a4ac363a1348b7c1ffb4d11
AlisonGrant <grantalison133@gmail.com>
2016-03-15 11:59:12 -0400

    js stuff

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,19 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-$("#getClients").on("click", function() {
+    $("#getClients").on("click", function() {
 
-  $.getJSON("http://AlisonGrant.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
+        $.getJSON("http://AlisonGrant.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
+            var html = "<table>" +
+              "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
-    $.each(data, function(index, item){
-      $("#data").append(item.name);
+            $.each(data, function(index, item) {
+                $("#data").append(item.name);
+              }) //each function
 
-    }) //each function
+              html += "</table>";
+              $("#data").append(html);
 
-    //alert(data);
-    //console.dir(data);
+            //alert(data);
+            //console.dir(data);
 
-  }) //get JSON
+          }) //get JSON
 
-}) //button
+      }) //button
 
-}) //ready
+  }) //ready
