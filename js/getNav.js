1e2f75eeab6281c63a7df944dd65378b2e6ebcdd
AlisonGrant <grantalison133@gmail.com>
2016-03-15 10:07:00 -0400

    style sheet

--- js/getNav.js
+++ js/getNav.js
@@ -3,6 +3,7 @@ $.get("http://AlisonGrant.github.io/partials/nav.html", function(data) {
   $(document).ready(function() {
 
     $(".container").prepend(data);
+    $(".container").fadeIn();
 
   })
 
