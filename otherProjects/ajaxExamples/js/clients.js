106627c206d4980228c608f338bee3e037a31ab7
AlisonGrant <grantalison133@gmail.com>
2016-03-15 11:39:52 -0400

    each

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,9 +1,19 @@
+$(document).ready(function(){
+
 $("#getClients").on("click", function() {
 
-$.getJSON("http://AlisonGrant.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data){
+  $.getJSON("http://AlisonGrant.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
 
-alert(data);
-console.dir(data);
-})
+    $.each(data, function(index, item){
+      $("#data").append(item.name);
 
-})
+    }) //each function
+
+    //alert(data);
+    //console.dir(data);
+
+  }) //get JSON
+
+}) //button
+
+}) //ready
