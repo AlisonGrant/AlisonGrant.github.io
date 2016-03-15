c41ca387697b701cb553661c077753285f265cd3
AlisonGrant <grantalison133@gmail.com>
2016-03-15 12:03:55 -0400

    tables

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -3,15 +3,21 @@ $(document).ready(function() {
     $("#getClients").on("click", function() {
 
         $.getJSON("http://AlisonGrant.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
-            var html = "<table>" +
+            var html = "<table class='table table-hover table-striped'>" +
               "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
             $.each(data, function(index, item) {
-                $("#data").append(item.name);
+
+                html += "<tr>" +
+                  "<td>" + item.name + "</td>" +
+                  "<td>" + item.email + "</td>" +
+                  "<td>" + item.company + "</td><" +
+                  "/tr>";
+
               }) //each function
 
-              html += "</table>";
-              $("#data").append(html);
+            html += "</table>";
+            $("#data").append(html);
 
             //alert(data);
             //console.dir(data);
